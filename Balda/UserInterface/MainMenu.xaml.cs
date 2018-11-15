0fe02117c770e6851dda895461e569a0b61510d4
Wilhelm <nightmare.v.94@gmail.com>
2018-11-15 15:30:49 +0500

    Изменена тема UI

---
+++ Balda/UserInterface/MainMenu.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Balda.UserInterface
+{
+	/// <summary>
+	/// Логика взаимодействия для MainMenu.xaml
+	/// </summary>
+	public partial class MainMenu : UserControl
+	{
+		public MainMenu()
+		{
+			InitializeComponent();
+		}
+	}
+}
