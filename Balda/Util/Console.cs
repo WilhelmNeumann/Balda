1cb99c21ac5e97813e4f0aac4275250d961b3e08
Wilhelm <nightmare.v.94@gmail.com>
2018-11-16 14:24:29 +0500

    Добавлен метод чтения с консоли

--- Balda/Util/Console.cs
+++ Balda/Util/Console.cs
@@ -8,18 +8,30 @@ namespace Balda.Util
 	/// </summary>
 	public static class Cons
 	{
+		/// <summary>
+		/// Вывод в консоль с переводом строки
+		/// </summary>
 		public static void Writeln(object obj)
 		{
 			AttachConsole(-1);
 			System.Console.WriteLine(obj.ToString());
 		}
 
+		/// <summary>
+		/// Вывод в консоль
+		/// </summary>
 		public static void Write(object obj)
 		{
 			AttachConsole(-1);
 			System.Console.WriteLine(obj.ToString());
 		}
 
+		public static void ReadLine()
+		{
+			AttachConsole(-1);
+			System.Console.ReadLine();
+		}
+		
 		[DllImport("Kernel32.dll")]
 		private static extern bool AttachConsole(int processId);
 	}
